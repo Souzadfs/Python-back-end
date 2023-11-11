a59d646152bac912a75988b8d07ec2bc58f37741
souzadfs <souzadf09@gmail.com>
2023-11-10 21:04:15 -0300

    is not

---
+++ flags_is_not.py
@@ -0,0 +1,13 @@
+codicao = True
+pass_if = None
+
+if condicao:
+    pass_if = True
+    print('Faça algo')
+else:
+    print('não faça algo')
+
+if pass_if is none:
+    print('não passou no if') 
+else:
+    print('passou no if') 
