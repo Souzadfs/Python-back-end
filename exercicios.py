4ea3387f03c00abe350d277fad24158e5eba6674
souzadfs <souzadf09@gmail.com>
2023-11-12 14:36:21 -0300

    exercios complete

--- exercicios.py
+++ exercicios.py
@@ -1,1 +1,14 @@
-horario = 
+horario = input(" Que horas são agora ? ")
+
+if horario.isdigit():
+    hra = float(horario)
+
+    if 0 <= hra <= 12:
+        print(f"Agora são {hra:.2f} horas, BOM DIA !!!")
+    elif 12 < hra < 18:
+        print(f"Agora são {hra:.2f} horas, BOA TARDE !!!")
+    elif 18 <= hra <= 23:
+        print(f"Agora são {hra:.2f} horas, BOA NOITE !!!")
+
+else:
+    print(f" Você digitou {horario}.")
