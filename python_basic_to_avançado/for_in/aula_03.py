73fae3dce56fcc0702c9fbb4c2489f307fa0f927
souzadfs <souzadf09@gmail.com>
2023-11-20 21:39:13 -0300

    process

---
+++ python_basic_to_avançado/for_in/aula_03.py
@@ -0,0 +1,13 @@
+for i in range(10):
+    if i == 4:
+        print('i é 4, pulando...')
+        continue
+    if i == 9:
+        print('i é 9, seu else não será executado')    
+        break
+
+    for j in range (1, 3):
+        print(i,j)
+
+else:
+    print('For completo com sucesso')
