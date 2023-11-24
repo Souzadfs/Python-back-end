372306e0c67472fc5112052e763c864eb0ef1f76
souzadfs <souzadf09@gmail.com>
2023-11-23 21:42:11 -0300

    condando

---
+++ python_basic_to_avançado/while/AULAS/aula_04.py
@@ -0,0 +1,34 @@
+# # string = 'valor qualquer'
+
+# # i = 0
+
+# # while i < len(string):
+# #     letra  = string[i]
+
+# #     if letra == ' ':
+# #         break
+
+# #     print(letra)
+
+# #     i += 1
+
+# # else:
+# #     print('Não encontrei um espaço na string')
+
+# print('Fora do while')
+
+""" while/else """
+string = 'Valorqualquer'
+
+i = 0
+while i < len(string):
+    letra = string[i]
+
+    if letra == ' ':
+        break
+
+    print(letra)
+    i += 1
+else:
+    print('Não encontrei um espaço na string.')
+print('Fora do while.')
