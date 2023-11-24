c85070d8c0f6444989fe54993b59e5d856b191b0
souzadfs <souzadf09@gmail.com>
2023-11-23 21:44:12 -0300

    corrigindo

--- python_basic_to_avançado/while/AULAS/aula_04.py
+++ python_basic_to_avançado/while/AULAS/aula_04.py
@@ -1,22 +1,3 @@
-# # string = 'valor qualquer'
-
-# # i = 0
-
-# # while i < len(string):
-# #     letra  = string[i]
-
-# #     if letra == ' ':
-# #         break
-
-# #     print(letra)
-
-# #     i += 1
-
-# # else:
-# #     print('Não encontrei um espaço na string')
-
-# print('Fora do while')
-
 """ while/else """
 string = 'Valorqualquer'
 
