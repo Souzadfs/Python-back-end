a740408526112abdb46d56b0a2811077cc17eb94
souzadfs <souzadf09@gmail.com>
2023-12-05 20:16:40 -0300

    pythonando

--- python_basic_to_avançado/for_in/Exerc_For/Exerc_01.py
+++ python_basic_to_avançado/for_in/Exerc_For/Exerc_01.py
@@ -1,5 +1,21 @@
+palavra_secr = 'perfume'
+letras_corretas = ''
 while True:
     letra = input('Digite uma letra: ')
-
+   
     if len(letra) > 1:
+        print('Digite apenas uma letra! ')
         continue
+
+    if letra in palavra_secr:
+        letras_corretas += letra
+  
+    palavra_formada = ''
+    for letra_secreta in palavra_secr:
+        if letra_secreta in letras_corretas:
+            palavra_formada += letra_secreta
+        
+        else:
+           palavra_formada += '*'
+  
+    print(palavra_formada)    
