958ebaf206525fdfdd4791c1686c4db10d490dfc
souzadfs <souzadf09@gmail.com>
2024-01-10 20:14:39 -0300

    lista

--- python_basic_to_avançado/Tipo_List/Intro_mutáveis/aula_03.py
+++ python_basic_to_avançado/Tipo_List/Intro_mutáveis/aula_03.py
@@ -3,9 +3,10 @@ lista.append('Denis')
 lista.append(100)
 lista.append(200)
 lista.append(300)
-lista.pop()
+#lista.pop()
 lista.append(400)
-del lista [0]
+lista.insert(0, 800)
+del lista [-1]
 
 print(lista)
 print(lista[7])
