f83e7885abe06d379e81d1c071801f80abaefaf3
souzadfs <souzadf09@gmail.com>
2024-02-14 21:22:56 -0300

    trying

---
+++ Teste_hanckerrank/test_01.py
@@ -0,0 +1,14 @@
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    if n %2 != 0:
+        print('Weird')
+    #if n %2 ==0 and n > 2 and n < 5:
+    if 2 < n < 5 and n % 2 == 0:
+        print('Not Weird')    
+    
+    if 6 < n < 20 and n % 2 == 0:
+        print('Weird_')
+
+    if 20 < n and n %2 == 0:
+        print('Not Weird__')    
---
+++ Teste_hanckerrank/test_02.py
@@ -0,0 +1,22 @@
+# if __name__ == '__main__':
+#     a = int(input('Digite o primeiro numero: '))
+#     b = int(input('Digite o segundo numero: '))
+    
+#     sum = a + b
+#     sub = a - b
+#     mult = a * b
+    
+#     print(sum)
+#     print(sub)
+#     print(mult)
+
+
+if __name__ == '__main__':
+    a = int(input('Digite o primeiro numero: '))
+    b = int(input('Digite o segundo numero: '))
+    
+    
+    print(a + b)
+    print(a - b)
+    print(a * b)
+    
